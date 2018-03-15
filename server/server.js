2e9b940677381212e1a61b66de7852f985593543
MatFunLab <matfunlab@gmail.com>
2018-03-15 15:43:07 +0100

    Added PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var {ObjectID} = require("mongodb");
+const _ = require("lodash");
+const express = require("express");
+const bodyParser = require("body-parser");
+const {ObjectID} = require("mongodb");
 
 const port = process.env.PORT || 3000;
 var {mongoose} = require("./db/mongoose");
@@ -62,6 +63,31 @@ app.get("/todos/:id", (req, res) => {
       res.status(400).send("Problem with communication with server");
     });
   });
+
+app.patch("/todos/:id", (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ["text", "completed"]);
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send("Status 404: Todo is not valid");
+  }
+        //if completed property is boolean and if its value is true
+  if(_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime(); // returns time in milisec
+  }else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new:true}).then((todo) => {
+    if(!todo) {
+      res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
